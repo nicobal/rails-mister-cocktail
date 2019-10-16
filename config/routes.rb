fff2d925fd2839dc030c600a2de5acffa7a2fdb6
Nicolas Ballian <nicobal@gmail.com>
2019-10-15 22:04:59 -0300

    done this shit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+  get 'doses/index'
+  get 'doses/new'
+  get 'doses/show'
+  get 'doses/edit'
+  get 'doses/update'
+  get 'cocktail/index'
+  get 'cocktail/new'
+  get 'cocktail/show'
+  get 'cocktail/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root "cocktails#index"
+
+  resources :cocktails do
+    resources :doses
+  end
 end
